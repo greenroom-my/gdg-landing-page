2a620d6583eb1a31e01eacf91e9fc942b7657560
louisyoong <louis@greenroom.com.my>
2019-03-21 11:38:34 +0800

    Add partner logo.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
-import { Component } from '@angular/core';
-import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import {Component} from '@angular/core';
+import {faArrowRight} from '@fortawesome/free-solid-svg-icons';
+import {faFacebookF} from '@fortawesome/free-brands-svg-icons';
+import {faTwitter} from '@fortawesome/free-brands-svg-icons';
+import {faInstagram} from '@fortawesome/free-brands-svg-icons';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector    : 'app-root',
+    templateUrl : './app.component.html',
+    styleUrls   : [ './app.component.scss' ]
 })
 
 export class AppComponent {
-  title = 'gdg';
+    title = 'gdg';
     faArrowRight = faArrowRight;
+    faFacebookF = faFacebookF;
+    faTwitter = faTwitter;
+    faInstagram = faInstagram;
 }
