8abaaa2f9bf9b41c29aaeced193a9944886b8e87
louisyoong <louis@greenroom.com.my>
2019-04-10 13:47:24 +0800

    Add blog component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,16 @@ import { EventListComponent } from './event-list/event-list.component';
 import {environment} from '../environments/environment';
 import {AngularFireModule} from '@angular/fire';
 import {AngularFirestoreModule} from '@angular/fire/firestore';
+import { BlogComponent } from './blog/blog.component';
+import { BannerComponent } from './banner/banner.component';
 
 @NgModule({
     declarations : [
         AppComponent,
         NavbarComponent,
-        EventListComponent
+        EventListComponent,
+        BlogComponent,
+        BannerComponent
     ],
     imports      : [
         AngularFireModule.initializeApp(environment.firebase),
